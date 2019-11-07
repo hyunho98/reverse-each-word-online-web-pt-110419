4137e651fcdabdde717523d85020faf8c650ce05
Alex Lee <hyunho98@gmail.com>
2019-11-07 23:04:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,5 @@
 def reverse_each_word(string)
-  string.split(" ").collect{|word| word = word.reverse}.join(" ")
+  array = string.split(" ")
+  output = array.collect{|word| word = word.reverse}
+  output.join(" ")
 end
